8b7d7f0e74b6146414b85a8ff9498d1c238cf91b
AaronTM44 <aaron44tm@gmail.com>
2022-12-11 03:50:12 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print numbers 
+ * Return 0
+ */
+int main(void)
+{
+	char n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

e0946faf15be96cc6a13841ce4cd3d9983422d44
AaronTM44 <aaron44tm@gmail.com>
2022-12-11 01:40:58 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+
+	c = 'A';
+	while(c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

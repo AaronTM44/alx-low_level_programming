e710cde8a63c598976131365c4143242584962c9
AaronTM44 <aaron44tm@gmail.com>
2022-12-11 02:25:44 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z') 
+	while (c <= 'z')
 	{
 		if ( c != 'q' && c != 'e')
-		  {
-			  putchar(c);
-		  }
+		{
+			putchar(c);
+		}	 
 		c++;
 	}
 	putchar('\n');

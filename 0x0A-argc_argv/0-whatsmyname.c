8f9856583213c2cdd4108e13a951a41d33a33b9c
AaronTM44 <aaron44tm@gmail.com>
2023-01-07 11:28:15 -0800

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: cout argument
+ * @argv: array of arg
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

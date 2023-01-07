663af5b626e0e6c4729afad89b7543c264fffe5f
AaronTM44 <aaron44tm@gmail.com>
2023-01-07 12:06:02 -0800

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,15 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = 0;
 
 	if (argc > 0)
 	{
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-		i++;
-	}
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
 	}
 	return (0);
 }
